1614dc563e8b15911c432c3a9fd60b69b95fd423
andrewpmize <andrewpmize@gmail.com>
2025-11-04 20:19:14 -0600

    Update service worker cache version to v13

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'chord-analyzer-v12';
+const CACHE = 'chord-analyzer-v13';
 const ASSETS = [
   './',
   './index.html',
@@ -26,6 +26,7 @@ self.addEventListener('fetch', (e) => {
     caches.match(request).then(res => res || fetch(request))
   );
 });
+
 
 
 
